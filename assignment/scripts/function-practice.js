145e0e43efffc39394954dbf40deee013725b56e
Chris Hess <chrishessmusic@gmail.com>
2021-08-04 13:15:10 -0500

    completed week 4 assignment part 1

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -15,7 +15,7 @@ console.log('1. Test - should say "Hello World!"', hello());
 // 2. Function to return a personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
 function helloName( name ) {
-  //return a phrase with passed in variable
+  //return a phrase with the passed in variable
   return `Hello there, ${name}!`;
 }
 // Remember to call the function to test
@@ -37,6 +37,7 @@ console.log('3. Test - should say "24"', multiplyThree(2,3,4));
 // 5. Function that will return true if a number is positive,
 //    or greater than zero, and false otherwise
 function isPositive( number ) {
+  //check if value is greater than 0 and return accordingly
   if ( number > 0 ){
     return true;
   }
@@ -54,23 +55,26 @@ console.log( '5. isPositive - should say false', isPositive(-3) );
 // 6. Function to return the _last_ item in an array. If the
 //    array is empty, return `undefined`.
 function getLast( array ) {
+  //check to see if there are any elements in the array
   if (array.length === 0){
     /*Not sure what the question is asking here. Should we return the string
-    'undefined' or the output is undefined? I will put the example for the latter
+    'undefined' or the output is undefined? I will put the code for the latter
     below. */
     return 'undefined';
   }
   else {
+    //return the last array element
     return array[array.length - 1];
   }
 }
 
 /* This would be the function if you just want the output to be undefined
-   if the array is empty.
+   if the array is empty. No if statement necessary.
 function getLast( array ) {
     return array[array.length - 1];
-}
-*/
+}*/
+
+//create array variables. not necessary but I wanted to show different ways of testing
 let arrayTest = [1, 2, 3, 4];
 let arrayTestEmpty = [];
 console.log('6. getLast with elements in array - should say "4"', getLast(arrayTest));
@@ -103,23 +107,65 @@ console.log('7. find Test - should return false', find(9, findArray));
 // 8. Function to check if a letter is the first letter in a
 //    string. Return true if it is, and false otherwise
 function isFirstLetter(letter, string) {
+  //compare the first array element to the letter given
+  if (string[0] === letter) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
-console.log( 'isFirstLetter - should say true', isFirstLetter('a', 'apple') );
-console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') );
+console.log( '8. isFirstLetter - should say true', isFirstLetter('a', 'apple') );
+console.log( '8. isFirstLetter - should say false', isFirstLetter('z', 'apple') );
 
 // 9. Function to return the sum of all numbers in an array
-function sumAll( ) {
+function sumAll(array) {
   let sum = 0
   // TODO: loop to add items
+  for (var i = 0; i < array.length; i++) {
+    //keep increasing sum total with array elements
+    sum = array[i] + sum;
+  }
   return sum;
 }
+console.log('9. sumAll - should say 130', sumAll([100, 20, 1, 4, 5]));
 
 // 10. Function to return a new array of all positive (greater than zero)
 //     numbers contained in an input array. If there are no positive numbers
 //     return an empty array. Note: The input array should not change.
 
+//
+function positiveNumbers(array) {
+  //create a new array
+  let newArray = [];
+  //loop through each array element
+  for (var i = 0; i < array.length; i++) {
+    //add only positive numbers to the new array
+    if (array[i] > 0) {
+      newArray.push(array[i]);
+    }
+  }
+  //return the new Array
+  return newArray;
+}
 
+console.log('10. positiveNumbers - should say [100, 4, 5]', positiveNumbers([100, -20, 0, 4, -10, 5]));
 
 // 11. Pick a problem from Edabit(https://edabit.com/) or
 //     CodeWars(https://www.codewars.com/). Then describe it
 //     here in a comment, write the function, and test it!
+
+//  Create a function that takes an array of numbers and return
+//  both the minimum and maximum numbers, in that order.
+//  Source: https://edabit.com/challenge/Q3n42rEWanZSTmsJm
+
+function minMax(arr) {
+  //transform and sort the array in ascending order
+	arr.sort(function(a, b){return a - b});
+  //create a new array to only contain the first and last elements
+	let newArray = [arr[0], arr[arr.length-1]];
+  //return the new array
+	return newArray;
+}
+
+console.log('11. minMax - should say [1, 5]', minMax([4, 1, 2, 5, 3]));
